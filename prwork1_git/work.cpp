40f7ef6ded98652664371c60997be1a8425a25c3
Dobby <eeermolina@edu.hse.ru>
2024-09-24 10:19:36 +0300

    a + b =

--- prwork1_git/work.cpp
+++ prwork1_git/work.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 
 int main() {
-	int a, b;
+	int a, b, c;
 
 	std::cout << "a: ";
 	std::cin >> a;
 
 	std::cout << "b: ";
 	std::cin >> b;
+
+	c = a + b;
+
+	std::cout << "a + b = "<< c;
+
 
 	return 0;
 }
